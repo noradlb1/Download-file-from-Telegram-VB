92b0dca3ef6e5eae9a140dad7fe6a20d5c383dc4
MONSTERMC <74623428+noradlb1@users.noreply.github.com>
2024-08-01 01:43:10 +0200

    Download-file-from-Telegram-VB
    Download-file-from-Telegram-VB

---
+++ Download file from TelegramM/Form1.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.filename_ext = New System.Windows.Forms.TextBox()
+        Me.FILE_ID = New System.Windows.Forms.TextBox()
+        Me.ButtonDownload = New System.Windows.Forms.Button()
+        Me.lblStatus = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'filename_ext
+        '
+        Me.filename_ext.Location = New System.Drawing.Point(314, 201)
+        Me.filename_ext.Name = "filename_ext"
+        Me.filename_ext.Size = New System.Drawing.Size(175, 20)
+        Me.filename_ext.TabIndex = 7
+        Me.filename_ext.Text = "filename.ext"
+        '
+        'FILE_ID
+        '
+        Me.FILE_ID.Location = New System.Drawing.Point(314, 168)
+        Me.FILE_ID.Name = "FILE_ID"
+        Me.FILE_ID.Size = New System.Drawing.Size(175, 20)
+        Me.FILE_ID.TabIndex = 6
+        Me.FILE_ID.Text = "FILE_ID"
+        '
+        'ButtonDownload
+        '
+        Me.ButtonDownload.Location = New System.Drawing.Point(311, 227)
+        Me.ButtonDownload.Name = "ButtonDownload"
+        Me.ButtonDownload.Size = New System.Drawing.Size(178, 23)
+        Me.ButtonDownload.TabIndex = 5
+        Me.ButtonDownload.Text = "ButtonDownload"
+        Me.ButtonDownload.UseVisualStyleBackColor = True
+        '
+        'lblStatus
+        '
+        Me.lblStatus.AutoSize = True
+        Me.lblStatus.Location = New System.Drawing.Point(311, 269)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(47, 13)
+        Me.lblStatus.TabIndex = 4
+        Me.lblStatus.Text = "lblStatus"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.filename_ext)
+        Me.Controls.Add(Me.FILE_ID)
+        Me.Controls.Add(Me.ButtonDownload)
+        Me.Controls.Add(Me.lblStatus)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents filename_ext As TextBox
+    Friend WithEvents FILE_ID As TextBox
+    Friend WithEvents ButtonDownload As Button
+    Friend WithEvents lblStatus As Label
+End Class
---
+++ Download file from TelegramM/Form1.vb
@@ -0,0 +1,48 @@
+﻿Imports System.Net.Http
+Imports System.Threading.Tasks
+Imports Newtonsoft.Json.Linq ' للتعامل مع JSON
+'========================
+'حيونا بتلغرام تفضلو
+'My Telegram : MONSTERMC
+'========================
+Public Class Form1
+    Private botToken As String = "7059390488:AAGXKbHiki8s4Z4dKqq2-a6yyd-VxhJw" 'التوكين الخاص بك
+
+    Private Async Function GetFilePathAsync(fileId As String) As Task(Of String)
+        Using client As New HttpClient()
+            Dim url = $"https://api.telegram.org/bot{7059390488:AAGXKbHiki8s4Z4dKqq2-a6yyd-VxhJw}/getFile?file_id={fileId}"
+            Dim response = Await client.GetStringAsync(url)
+            Dim json = JObject.Parse(response)
+            Dim filePath = json("result")("file_path").ToString()
+            Return filePath
+        End Using
+    End Function
+
+    Private Async Function DownloadFileAsync(fileId As String, fileName As String) As Task
+        Try
+            ' الحصول على file_path
+            Dim filePath = Await GetFilePathAsync(fileId)
+            Dim fileUrl = $"https://api.telegram.org/file/bot{7059390488:AAGXKbHiki8s4Z4dKqq2-a6yyd-VxhJw}/{filePath}"
+
+            ' تحميل الملف من رابط التحميل
+            Using client As New HttpClient()
+                Dim fileResponse = Await client.GetAsync(fileUrl)
+                fileResponse.EnsureSuccessStatusCode()
+
+                Dim savePath = System.IO.Path.Combine(Application.StartupPath, fileName)
+                Using fileStream = New IO.FileStream(savePath, IO.FileMode.Create, IO.FileAccess.Write, IO.FileShare.None)
+                    Await fileResponse.Content.CopyToAsync(fileStream)
+                End Using
+
+                MessageBox.Show($"تم تحميل الملف: {fileName} في {savePath}")
+            End Using
+        Catch ex As Exception
+            MessageBox.Show($"حدث خطأ أثناء تحميل الملف: {ex.Message}")
+        End Try
+    End Function
+
+    ' استخدام المثال:
+    Private Async Sub ButtonDownload_Click(sender As Object, e As EventArgs) Handles ButtonDownload.Click
+
+    End Sub
+End Class
